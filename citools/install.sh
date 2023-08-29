6ed15b08d8a21a9ec59831e8b327a7341b55ec37
Shell.Xu <shell909090@gmail.com>
2023-08-29 16:52:43 +0800

    change download from github pages

--- citools/install.sh
+++ citools/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -fsSL -o citools "https://testany-io.bitbucket.io/citools/citools"
+curl -fsSL -o citools "https://testany-io.github.io/citools/citools"
 chmod +x citools
 
 curl -fsSL -o semver "https://raw.githubusercontent.com/fsaintjacques/semver-tool/master/src/semver"
